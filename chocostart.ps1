2ffdd8e15ef409ace0b5ff07c7b163659ff8cd0d
Henrique Silva <38558536+henriwasd@users.noreply.github.com>
2019-09-16 11:54:00 -0300

    Update chocostart.ps1

--- chocostart.ps1
+++ chocostart.ps1
@@ -2,18 +2,18 @@ Set-ExecutionPolicy unrestricted -Scope Process -Force; iex ((New-Object System.
 
 choco install explorer-winconfig --params "'/SHOWHIDDEN:yes /SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWENCRYPTED:no /SHOWCHECKBOXES:yes /USESHARINGWIZARD:no /USEVIEW:details'"
 choco install desktopicons-winconfig --params "'/Desktop:NO /UserFiles:NO /ControlPanel:NO /Network:NO /RecycleBin:YES /OneDrive:NO'"
+choco install taskbar-winconfig --params "' /AUTOTRAY:YES /CORTANA:no /COMBINED:yes /AUTOTRAY:YES /STORE:yes /TASKVIEW:no /KEYBOARD:no /USEPOWERSHELL:yes'"
 
 choco feature enable -n=allowGlobalConfirmation
 
-choco install chocolatey 
+choco install chocolatey
 choco install listary
 choco install googlechrome
-choco install firefox
 choco install 7zip
 choco install git
 choco install vcredist2017
 choco install cmder
-choco install vscode 
+choco install vscode
 choco install steam
 choco install uplay
 choco install whatsapp
@@ -21,7 +21,6 @@ choco install sublimetext3
 choco install yumi
 choco install nodejs
 choco install spotify
-choco install intellijidea-community
 choco install openjdk
 choco install origin
 choco install qbittorrent
@@ -34,7 +33,6 @@ choco install dbeaver
 choco install yumi
 choco install telegram
 choco install discord
-choco install brackets
 choco install vibrancegui
 choco install firacode
 choco install epicgameslauncher
