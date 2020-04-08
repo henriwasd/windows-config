d42848803f212297bc0b84be6c27bb08673f4315
Henrique Silva <38558536+henriwasd@users.noreply.github.com>
2020-04-08 07:33:54 -0300

    Update pc-henri.ps1

--- pc-henri.ps1
+++ pc-henri.ps1
@@ -1,1 +1,37 @@
+Set-ExecutionPolicy unrestricted -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco feature enable -n=allowGlobalConfirmation
+
+choco install explorer-winconfig --params "'/SHOWHIDDEN:yes /SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWENCRYPTED:no /SHOWCHECKBOXES:yes /USESHARINGWIZARD:no /USEVIEW:details'"
+choco install desktopicons-winconfig --params "'/Desktop:NO /UserFiles:NO /ControlPanel:NO /Network:NO /RecycleBin:YES /OneDrive:NO'"
+choco install taskbar-winconfig --params "' /AUTOTRAY:YES /CORTANA:no /COMBINED:yes /AUTOTRAY:YES /STORE:yes /TASKVIEW:no /KEYBOARD:no /USEPOWERSHELL:yes'"
+
+choco install chocolatey
+choco install googlechrome
+choco install 7zip
+choco install git
+choco install vscode
+choco install steam
+choco install uplay
+choco install whatsapp
+choco install sublimetext3
+choco install nodejs
+choco install spotify
+choco install openjdk
+choco install origin
+choco install postgresql
+choco install discord
+choco install firacode
+choco install epicgameslauncher
+choco install teamspeak
+choco install geforce-experience
+choco install chocolatey-windowsupdate.extension
+choco install autohotkey
+choco install dotnetcore-sdk
+choco install visualstudio-installer
+choco install chocolateygui
+choco install yarn
+choco install -y nodejs.install python2 jdk8
+choco install tortoisegit
+
+choco install choco-cleaner 
