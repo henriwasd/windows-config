be5f7ae3e6098193ace5218e87967f885fd00c50
Henrique Silva <38558536+henriwasd@users.noreply.github.com>
2020-04-08 07:37:30 -0300

    Update pc-henri.ps1

--- pc-henri.ps1
+++ pc-henri.ps1
@@ -3,8 +3,8 @@ Set-ExecutionPolicy unrestricted -Scope Process -Force; iex ((New-Object System.
 choco feature enable -n=allowGlobalConfirmation
 
 choco install explorer-winconfig --params "'/SHOWHIDDEN:yes /SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWENCRYPTED:no /SHOWCHECKBOXES:yes /USESHARINGWIZARD:no /USEVIEW:details'"
-choco install desktopicons-winconfig --params "'/Desktop:NO /UserFiles:NO /ControlPanel:NO /Network:NO /RecycleBin:YES /OneDrive:NO'"
-choco install taskbar-winconfig --params "' /AUTOTRAY:YES /CORTANA:no /COMBINED:yes /AUTOTRAY:YES /STORE:yes /TASKVIEW:no /KEYBOARD:no /USEPOWERSHELL:yes'"
+choco install desktopicons-winconfig --params "'/Desktop:NO /UserFiles:YES /ControlPanel:YES /Network:NO /RecycleBin:YES /OneDrive:NO'"
+choco install taskbar-winconfig --params "' /AUTOTRAY:YES /CORTANA:no /COMBINED:YES /AUTOTRAY:YES /STORE:NO /TASKVIEW:NO /KEYBOARD:NO /USEPOWERSHELL:YES'"
 
 choco install chocolatey
 choco install googlechrome
