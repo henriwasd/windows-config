278bbd09a8bf5cca53c08093409338df78316c48
Henrique Silva <henriwasd@outlook.com>
2020-06-05 20:44:32 -0300

    Customizando o git bash

--- git-prompt.sh
+++ git-prompt.sh
@@ -1,6 +1,10 @@
 # set window title
 PS1='\[\033]0;Git-Bash: ${PWD//[^[:ascii:]]/?}\007\]'
 PS1="$PS1"'\n'                 # new line
+PS1="$PS1"'\[\033[35m\]'       # change to purple
+PS1="$PS1"'\u@\h '             # user@host<space>
+PS1="$PS1"'\[\033[36m\]'       # change to brownish yellow
+PS1="$PS1"'\w '				   # current working directory
 PS1="$PS1"'\[\033[32m\]'       # change to green
 PS1="$PS1"'➜ '                # ➜
 PS1="$PS1"'\[\033[0m\]'        # change color
