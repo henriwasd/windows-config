bc79571724ae5d05df20d0984b836e48879b0d09
Henrique Silva <henriwasd@outlook.com>
2020-06-05 20:52:56 -0300

    Customizando o git bash Final

--- git-prompt.sh
+++ git-prompt.sh
@@ -1,11 +1,52 @@
-# set window title
+COLOR_RED="\033[0;31m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_GREEN="\033[0;32m"
+COLOR_OCHRE="\033[38;5;95m"
+COLOR_BLUE="\033[0;34m"
+COLOR_WHITE="\033[0;37m"
+COLOR_RESET="\033[0m"
+
+function git_color {
+  local git_status="$(git status 2> /dev/null)"
+
+  if [[ ! $git_status =~ "working directory clean" ]]; then
+    echo -e $COLOR_RED
+  elif [[ $git_status =~ "Your branch is ahead of" ]]; then
+    echo -e $COLOR_YELLOW
+  elif [[ $git_status =~ "nothing to commit" ]]; then
+    echo -e $COLOR_GREEN
+  else
+    echo -e $COLOR_OCHRE
+  fi
+}
+
+function git_branch {
+  local git_status="$(git status 2> /dev/null)"
+  local on_branch="On branch ([^${IFS}]*)"
+  local on_commit="HEAD detached at ([^${IFS}]*)"
+
+  if [[ $git_status =~ $on_branch ]]; then
+    local branch=${BASH_REMATCH[1]}
+    echo "($branch)"
+  elif [[ $git_status =~ $on_commit ]]; then
+    local commit=${BASH_REMATCH[1]}
+    echo "($commit)"
+  fi
+}
+
+PS1='\[\033]0;Git-Bash: ${PWD//[^[:ascii:]]/?}\007\]'
+PS1="$PS1"'\[\033[35m\]'       # change to purple
+PS1="$PS1"'\u@\h '             # user@host<space>
 PS1='\[\033]0;Git-Bash: ${PWD//[^[:ascii:]]/?}\007\]'
 PS1="$PS1"'\n'                 # new line
 PS1="$PS1"'\[\033[35m\]'       # change to purple
 PS1="$PS1"'\u@\h '             # user@host<space>
 PS1="$PS1"'\[\033[36m\]'       # change to brownish yellow
 PS1="$PS1"'\w '				   # current working directory
+PS1+="\[\$(git_color)\]"        # colors git status
+PS1+="\$(git_branch)"           # prints current branch
 PS1="$PS1"'\[\033[32m\]'       # change to green
 PS1="$PS1"'➜ '                # ➜
 PS1="$PS1"'\[\033[0m\]'        # change color
 MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
+export PS1
