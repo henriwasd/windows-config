decc559aca36a28fa0a9958a32a81a8b3706eded
Henrique Silva <38558536+henriwasd@users.noreply.github.com>
2020-06-25 15:17:50 -0300

    Update pc-henri.ps1

--- pc-henri.ps1
+++ pc-henri.ps1
@@ -4,7 +4,7 @@ choco feature enable -n=allowGlobalConfirmation
 
 choco install explorer-winconfig --params "'/SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWHIDDEN:yes /SHOWCHECKBOXES:no /SHOWENCRYPTED:yes /SHOWPREVIEWPANE:yes /SHOWDETAILSPANE:yes /SHOWDRIVESNOMEDIA:yes /USEVIEW:details /USESHARINGWIZARD:yes'"
 choco install desktopicons-winconfig --params "' /RecycleBin:YES  /ControlPanel:YES /UserFiles:YES /Computer:YES'"
-choco install taskbar-winconfig --params "' /AUTOTRAY:YES /CORTANA:no /COMBINED:yes /AUTOTRAY:YES /STORE:YES /TASKVIEW:NO /KEYBOARD:NO /USEPOWERSHELL:YES'"
+choco install taskbar-winconfig --params "' /CORTANA:no /COMBINED:yes /AUTOTRAY:YES /STORE:NO /TASKVIEW:NO /KEYBOARD:NO /USEPOWERSHELL:YES'"
 
 choco install microsoft-windows-terminal
 choco install geforce-experience
@@ -15,7 +15,7 @@ choco install googlechrome
 choco install firefox
 choco install 7zip
 choco install git
-choco install vscode-insiders
+choco install vscode
 choco install steam
 choco install uplay
 choco install whatsapp
