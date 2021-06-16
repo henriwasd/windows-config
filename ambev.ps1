a38a33bd2ae27395917dc999ab7c67624786c2b6
Henrique Silva <38558536+henriwasd@users.noreply.github.com>
2021-06-16 16:06:35 -0300

    Update ambev.ps1

--- ambev.ps1
+++ ambev.ps1
@@ -1,5 +1,3 @@
-Set-ExecutionPolicy unrestricted -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
 choco feature enable -n=allowGlobalConfirmation
 
 choco install explorer-winconfig --params "'/SHOWHIDDEN:yes /SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWENCRYPTED:no /SHOWCHECKBOXES:yes /USESHARINGWIZARD:no /USEVIEW:details'"
