ba5b2105a8703317ee1f9599c0dba962047d183e
Henrique Silva <38558536+henriwasd@users.noreply.github.com>
2021-07-20 20:03:00 -0300

    Update pc-henri.ps1

--- pc-henri.ps1
+++ pc-henri.ps1
@@ -1,31 +1,32 @@
 Set-ExecutionPolicy unrestricted -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco feature enable -n=allowGlobalConfirmation
+choco feature enable -n=allowEmptyChecksums
 
 choco install explorer-winconfig --params "'/SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWHIDDEN:yes /SHOWCHECKBOXES:yes /SHOWENCRYPTED:yes /SHOWPREVIEWPANE:yes /SHOWDETAILSPANE:yes /SHOWDRIVESNOMEDIA:yes /USEVIEW:details /USESHARINGWIZARD:yes'"
 choco install desktopicons-winconfig --params "' /RecycleBin:YES /ControlPanel:YES /UserFiles:YES /Computer:YES'"
 choco install taskbar-winconfig --params "' /CORTANA:no /COMBINED:no /AUTOTRAY:YES /STORE:NO /TASKVIEW:NO /KEYBOARD:NO /USEPOWERSHELL:NO /SIZE:small'"
 
 choco install chocolateygui
-choco install anydesk --allow-empty-checksums
-choco install chocolatey --allow-empty-checksums
-choco install googlechrome --allow-empty-checksums
-choco install 7zip --allow-empty-checksums
-choco install git --allow-empty-checksums
-choco install vscode --allow-empty-checksums
-choco install discord --allow-empty-checksums
-choco install firacode --allow-empty-checksums
-choco install chocolatey-windowsupdate.extension --allow-empty-checksums
-choco install obs-studio --allow-empty-checksums
-choco install qbittorrent --allow-empty-checksums
-choco install droidcamclient --allow-empty-checksums
-choco install everything --allow-empty-checksums
-choco install insomnia-rest-api-client --allow-empty-checksums
-choco install dbeaver --allow-empty-checksums
-choco install yarn --allow-empty-checksums
-choco install obs-virtualcam --allow-empty-checksums
-choco install ecm --allow-empty-checksums
-choco install sublimetext3 --allow-empty-checksums
+choco install anydesk
+choco install chocolatey 
+choco install googlechrome
+choco install 7zip 
+choco install git
+choco install vscode 
+choco install discord
+choco install firacode
+choco install chocolatey-windowsupdate.extension
+choco install obs-studio
+choco install qbittorrent 
+choco install droidcamclient
+choco install everything
+choco install insomnia-rest-api-client
+choco install dbeaver
+choco install yarn
+choco install obs-virtualcam
+choco install ecm
+choco install sublimetext3
 choco install nvm
 choco install powershell-core
 choco install poshgit
