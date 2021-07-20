9b1b4044be3cc29882f215ef2c120314bc90373c
Henrique Silva <38558536+henriwasd@users.noreply.github.com>
2021-07-20 19:55:58 -0300

    Update pc-henri.ps1

--- pc-henri.ps1
+++ pc-henri.ps1
@@ -6,25 +6,25 @@ choco install explorer-winconfig --params "'/SHOWEXTENSIONS:yes /SHOWFULLPATH:ye
 choco install desktopicons-winconfig --params "' /RecycleBin:YES /ControlPanel:YES /UserFiles:YES /Computer:YES'"
 choco install taskbar-winconfig --params "' /CORTANA:no /COMBINED:no /AUTOTRAY:YES /STORE:NO /TASKVIEW:NO /KEYBOARD:NO /USEPOWERSHELL:NO /SIZE:small'"
 
-choco install anydesk 
-choco install chocolatey
-choco install googlechrome
-choco install 7zip
-choco install git
-choco install vscode
-choco install discord
-choco install firacode
-choco install chocolatey-windowsupdate.extension
-choco install obs-studio
-choco install qbittorrent
-choco install droidcamclient
-choco install everything
-choco install insomnia-rest-api-client
-choco install dbeaver
-choco install yarn
-choco install obs-virtualcam
-choco install ecm
-choco install sublimetext3
+choco install anydesk --allow-empty-checksums
+choco install chocolatey --allow-empty-checksums
+choco install googlechrome --allow-empty-checksums
+choco install 7zip --allow-empty-checksums
+choco install git --allow-empty-checksums
+choco install vscode --allow-empty-checksums
+choco install discord --allow-empty-checksums
+choco install firacode --allow-empty-checksums
+choco install chocolatey-windowsupdate.extension --allow-empty-checksums
+choco install obs-studio --allow-empty-checksums
+choco install qbittorrent --allow-empty-checksums
+choco install droidcamclient --allow-empty-checksums
+choco install everything --allow-empty-checksums
+choco install insomnia-rest-api-client --allow-empty-checksums
+choco install dbeaver --allow-empty-checksums
+choco install yarn --allow-empty-checksums
+choco install obs-virtualcam --allow-empty-checksums
+choco install ecm --allow-empty-checksums
+choco install sublimetext3 --allow-empty-checksums
 choco install nvm
 choco install powershell-core
 choco install poshgit
@@ -36,4 +36,5 @@ choco install t-clock
 choco install whatsapp
 choco install telegram
 choco install vmware-workstation-player
+choco install spotify
 choco install choco-cleaner
