6bd49454b1aec45c848cd52415527f71b91d577c
Henrique Silva <38558536+henriwasd@users.noreply.github.com>
2021-08-12 16:06:20 -0300

    Update ambev.ps1

--- ambev.ps1
+++ ambev.ps1
@@ -2,11 +2,6 @@ Set-ExecutionPolicy unrestricted -Scope Process -Force; iex ((New-Object System.
 
 choco feature enable -n=allowGlobalConfirmation
 choco feature enable -n=allowEmptyChecksums
-
-choco install explorer-winconfig --params "'/SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWHIDDEN:yes /SHOWCHECKBOXES:yes /SHOWENCRYPTED:yes /SHOWPREVIEWPANE:yes /SHOWDETAILSPANE:yes /SHOWDRIVESNOMEDIA:yes /USEVIEW:details /USESHARINGWIZARD:yes'"
-choco install desktopicons-winconfig --params "' /RecycleBin:YES /ControlPanel:YES /UserFiles:YES /Computer:YES'"
-choco install taskbar-winconfig --params "' /CORTANA:no /COMBINED:yes /AUTOTRAY:YES /STORE:NO /TASKVIEW:NO /KEYBOARD:NO /USEPOWERSHELL:NO /SIZE:small'"
-
 choco install postgresql
 choco install screentogif
 choco install autohotkey
