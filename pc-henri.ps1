44b6039e0056ce6e8f7f308384068966121dce11
Henrique Silva <38558536+henriwasd@users.noreply.github.com>
2021-08-20 20:02:21 -0300

    Update pc-henri.ps1

--- pc-henri.ps1
+++ pc-henri.ps1
@@ -40,6 +40,7 @@ choco install scrcpy
 choco install vim
 choco install docker-desktop
 choco install minikube
+choco install eartrumpet
 choco install choco-cleaner
 
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
