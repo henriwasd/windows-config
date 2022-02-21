a674d81d48fa51e5e54edf98ebb54aecb82e3b1a
Henrique Silva <henriwasd@gmail.com>
2022-02-21 03:37:54 -0300

    Ajuste de config do windows

--- pc-henri.ps1
+++ pc-henri.ps1
@@ -1,5 +1,9 @@
 Set-ExecutionPolicy unrestricted -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco install explorer-winconfig --params "'/SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWHIDDEN:yes /SHOWCHECKBOXES:no /SHOWENCRYPTED:yes /SHOWPREVIEWPANE:yes /SHOWDETAILSPANE:yes /SHOWDRIVESNOMEDIA:yes /USEVIEW:details /USESHARINGWIZARD:yes'"
+choco install desktopicons-winconfig --params "' /RecycleBin:no  /ControlPanel:no /UserFiles:no /Computer:no'"
+choco install taskbar-winconfig --params "' /CORTANA:no /COMBINED:no /AUTOTRAY:NO /STORE:NO /TASKVIEW:NO /KEYBOARD:NO /USEPOWERSHELL:YES /SIZE:big'"
+
 choco feature enable -n=allowGlobalConfirmation
 choco feature enable -n=allowEmptyChecksums
 choco feature disable -n=checksumFiles
@@ -11,29 +15,18 @@ choco install everything
 choco install googlechrome
 choco install 7zip 
 choco install git
-# choco install vscode-insiders
 choco install vscode
-choco install discord
 choco install chocolatey-windowsupdate.extension
 choco install obs-studio
-# choco install qbittorrent 
-choco install droidcamclient
 choco install insomnia-rest-api-client
 choco install dbeaver
 choco install yarn
 choco install obs-virtualcam
 choco install ecm
-choco install sublimetext3
 choco install nvm
-choco install lightshot
 choco install hwmonitor
-choco install vim
 choco install minikube
 choco install eartrumpet
-choco install epicgameslauncher
-choco install steam-client
-choco install battle.net
-choco install ubisoft-connect
 choco install scrcpy
 choco install motrix
 choco install onlyoffice
@@ -46,14 +39,8 @@ choco install vlc
 choco install kdenlive
 choco install oh-my-posh
 choco install poshgit
-choco install whatsapp
-choco install telegram
 choco install streamlabs-obs
 choco install microsoft-windows-terminal
 choco install postman
-choco install lightshot
 choco install cascadia-code-nerd-font
 choco install choco-cleaner
-
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni $HOME/vimfiles/autoload/plug.vim -Force
